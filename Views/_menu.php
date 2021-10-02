32a908cd120477c0e8a93bbceb04689c92417328
André Simiquelli <andresimiquelli@hotmail.com>
2021-10-02 11:44:55 -0300

    Upload do código

--- Views/_menu.php
+++ Views/_menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #7137c8;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">      
     </a>
